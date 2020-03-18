1dd274beac8e008c815bd843b9c8b853dc967bcf
zscgrhg <zscgrhg@qq.com>
2020-03-18 23:33:44 +0800

    oho

--- src/main/java/com/etz/replay/unit/context/ParamWriterImpl.java
+++ src/main/java/com/etz/replay/unit/context/ParamWriterImpl.java
@@ -7,16 +7,23 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class ParamWriterImpl implements ParamWriter {
-    public static final File workspace = new File("data").toPath().resolve(Long.toHexString(System.currentTimeMillis())).toFile();
 
-   static {
-       if (!workspace.exists()) {
-           workspace.mkdirs();
-       }
-   }
+    public static final File workspace = new File("data").toPath().resolve(ymdHmsOfNow()).toFile();
+
+    static {
+        if (!workspace.exists()) {
+            workspace.mkdirs();
+        }
+    }
+
+    public static String ymdHmsOfNow() {
+        return new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+    }
 
     @Override
     @SneakyThrows
